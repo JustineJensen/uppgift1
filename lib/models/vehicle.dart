4ca95049b5f1b5cb7d5827b2e4a011af6d676ea5
JustineJensen <justinejensen1@gmail.com>
2025-02-03 22:50:06 +0100

    changed String on typ to enum class VehicleType

--- lib/models/vehicle.dart
+++ lib/models/vehicle.dart
@@ -1,23 +1,24 @@
 import 'package:uppgift1/models/person.dart';
+import 'package:uppgift1/models/vehicleType.dart';
 
 abstract class Vehicle {
   late String _registreringsnummer;
-  late String _typ;
+  late Vehicletype _typ;
   late Person _owner;
  
  // constructor
-  Vehicle({required String registreringsnummer, required  String typ, required Person owner})
+  Vehicle({required String registreringsnummer, required  Vehicletype typ, required Person owner})
   :_registreringsnummer = registreringsnummer,
   _typ = typ,
   _owner = owner;
 
 //Getters
 String get registreringsnummer => _registreringsnummer;
-String get typ => _typ;
+Vehicletype get typ => _typ;
 Person get owner => _owner;
 //Setters
 set registreringsnummer (String registreringsnummer)=>_registreringsnummer =registreringsnummer;
-set typ(String typ) => _typ = typ;
+set typ(Vehicletype typ) => _typ = typ;
 set owner (Person owner) => _owner = owner;
 
 @override

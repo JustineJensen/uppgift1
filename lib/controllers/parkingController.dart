96c505866f1e58d20ac1a6f8e4f3ea170c0f0bfe
JustineJensen <justinejensen1@gmail.com>
2025-02-10 10:24:40 +0100

    Feat: added VehicleController

--- lib/controllers/parkingController.dart
+++ lib/controllers/parkingController.dart
@@ -3,7 +3,7 @@ import 'package:uppgift1/models/parking.dart';
 
 class Parkingcontroller extends Repository<Parking,int> {
   final List<Parking> _parking =[];
-
+  int _nextId = 1;
   @override
  Parking add( Parking parking) {
    _parking.add(parking);
@@ -19,18 +19,23 @@ class Parkingcontroller extends Repository<Parking,int> {
   @override
   List<Parking> findAll() {
     return _parking;
-  
   }
 
   @override
   Parking findById(int id) {
     return _parking.firstWhere((parking)=>parking.id ==id, orElse:()=>
     throw Exception("Parking med ID $id hittades inte"),);
-   
   }
 
   @override
-  void update(entity) {
-    // TODO: implement update
+  void update(Parking entity) {
+    int index = _parking.indexWhere((p)=>p.id == entity.id);
+    if(index !=-1){
+      _parking[index]=entity;
+    }else{
+      int getNextId(){
+        return _nextId;
+      }
+    }
   }
 }
---
+++ lib/controllers/parkingSpaceController.dart
@@ -0,0 +1,32 @@
+import 'package:uppgift1/controllers/repository.dart';
+import 'package:uppgift1/models/parkingSpace.dart';
+
+class Parkingspacecontroller extends Repository<ParkingSpace,int> {
+  @override
+  ParkingSpace add(ParkingSpace entity) {
+    // TODO: implement add
+    throw UnimplementedError();
+  }
+
+  @override
+  void deleteById(int id) {
+    // TODO: implement deleteById
+  }
+
+  @override
+  List<ParkingSpace> findAll() {
+    // TODO: implement findAll
+    throw UnimplementedError();
+  }
+
+  @override
+  ParkingSpace findById(int id) {
+    // TODO: implement findById
+    throw UnimplementedError();
+  }
+
+  @override
+  void update(ParkingSpace entity) {
+    // TODO: implement update
+  }
+}
